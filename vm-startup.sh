68c29903d903e986255fd53464170eac620d074a
syhsiao <82127503+syhsiao@users.noreply.github.com>
2026-04-02 09:26:41 +0800

    Change tmux prefix to ctrl-a in startup script

--- vm-startup.sh
+++ vm-startup.sh
@@ -41,6 +41,14 @@ bind-key -n 'C-h' if-shell "\$is_vim" 'send-keys C-h' 'select-pane -L'
 bind-key -n 'C-j' if-shell "\$is_vim" 'send-keys C-j' 'select-pane -D'
 bind-key -n 'C-k' if-shell "\$is_vim" 'send-keys C-k' 'select-pane -U'
 bind-key -n 'C-l' if-shell "\$is_vim" 'send-keys C-l' 'select-pane -R'
+
+# 將前綴鍵改為 Ctrl + a:      
+# 1. 取消原本的 Ctrl + b 綁定      
+unbind C-b      
+# 2. 設定新的前綴鍵為 Ctrl + a      
+set -g prefix C-a      
+# 3. 確保按兩次 Ctrl + a 可以把這個按鍵傳送給內層程式 (例如在 Bash 跳到行首)                                           
+bind C-a send-prefix  
 EOF
 
 # --- 8. Set Environment Variables ---
