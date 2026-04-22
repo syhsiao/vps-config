25c2dd6e79c99a224e3636faacb7c320ce4ec280
syhsiao <82127503+syhsiao@users.noreply.github.com>
2026-04-22 08:51:14 +0800

    fix: change cs path from absolute root to relative in startup script scala module

--- scripts/install-scala.sh
+++ scripts/install-scala.sh
@@ -4,5 +4,5 @@
 curl -fL https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz | gzip -d > cs
 chmod +x cs
 ./cs setup --yes --jvm 21
-/cs install metals
+./cs install metals
 mv cs /usr/local/bin/
